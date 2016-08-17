fbed8fe020aca4fc0902a295f5e09a1deedf7416
petersonben50 <petersonben50@gmail.com>
2016-08-17 15:59:04 -0500

    Mothur's helper file updated. This file now renames the fasta, shared, list, names, and group files from the mothur workflow and moves them into dataEdited, both on my computer and HD. Also have added a script to reformat the fasta file so that it works in Robin's workflow and matches the shared file.

--- code/bash/mothurs_little_helper.sh
+++ code/bash/mothurs_little_helper.sh
@@ -6,11 +6,33 @@
 # We'll also need to convert the shared file to an OTU file.
 
 # First we'll rename and copy the files into the dataEdited file on BDPLabHD
-cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.trim.contigs.good.unique.good.filter.unique.precluster.pick.an.unique_list.0.03.subsample.shared /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.shared
-cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.trim.contigs.good.unique.good.filter.unique.precluster.pick.fasta /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.fasta
+# Shared file - To know the counts per sample
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.trim.contigs.good.unique.good.filter.unique.precluster.pick.an.unique_list.shared /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.shared
+# Fasta file - Obviously.
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.trim.contigs.good.unique.good.filter.unique.precluster.pick.an.unique_list.0.03.rep.fasta /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.longname.fasta
+# List file - List of sequences that go with each OTU
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.trim.contigs.good.unique.good.filter.unique.precluster.pick.an.unique_list.list /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.list
+# Name file - For use in the taxonomy summary later
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.trim.contigs.good.unique.good.filter.unique.precluster.pick.an.unique_list.0.03.rep.names /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.names
+# Group file - For use in the taxonomy summary later
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.contigs.good.pick.groups /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.groups
 
 # Let's also move these files onto my computer in the dataEdited directory
+# Shared file
 cp /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.shared ./dataEdited/WAVES.final.shared
+# Fasta file
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.longname.fasta ./dataEdited/WAVES.final.longname.fasta
+# List file
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.list ./dataEdited/WAVES.final.list
+# Name file - For use in the taxonomy summary later
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.names ./dataEdited/WAVES.final.names
+# Group file - For use in the taxonomy summary later
+cp /Volumes/BDPLabHD/data/WAVES/dataEdited/mothur/WAVES.final.groups ./dataEdited/WAVES.final.groups
+
+# Reformat the fasta file names so the sequence name is >OtuXXXX
+sed 's/.*\(Otu[0-9]*\).*/>\1/' /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.longname.fasta > /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.fasta
+
+# Also move this code onto BDPLabHD
 cp /Volumes/BDPLabHD/data/WAVES/dataEdited/WAVES.final.fasta ./dataEdited/WAVES.final.fasta
 
 # Now reformat the shared file into an OTU table using an R script that I wrote.
